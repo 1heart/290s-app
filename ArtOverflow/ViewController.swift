40e4ce7cb37624c409f3d2bd02e95fd2befd0683
Yixin Lin <yixinlin@Yixins-Mac-Pro.local>
2015-02-15 12:00:52 -0500

    Initial Commit

---
+++ ArtOverflow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArtOverflow
+//
+//  Created by Yixin Lin on 2/15/15.
+//  Copyright (c) 2015 art290s. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
